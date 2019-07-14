5e2c09cb5eff09e7e134f161a1307b456beb77be
rkej <rahulkejriwal18@gmail.com>
2019-07-14 01:25:47 -0400

    Added Registration routing - frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import SignIn from './SignIn';
+import Register from './Register'
 import * as serviceWorker from './serviceWorker';
 const routing = (
 <Router>
 <div>
     <Route exact path = "/" component = {App}/>
     <Route exact path = '/signin' component = {SignIn}/>
+    <Route exact path = '/register' component = {Register}/>
       </div>
       </Router>);
 
@@ -18,4 +20,4 @@ ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister(); 
