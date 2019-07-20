131f653127c6efd06d915b275667dbc66966835e
Krish G <1001alphatest@gmail.com>
2019-07-20 23:53:41 +0530

    re-styled signIn and register

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,55 +1,69 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Card, Button, CardHeader, CardFooter, CardBody,
     CardTitle, CardText, Col, Row, Form, FormGroup, Label, Input, FormText
 } from 'reactstrap';
 import App from './App';
 
-export default class Register extends Component {
+export default class SignIn extends Component {
+
     render() {
-        const { classes } = this.props;
-        return <div> 
-            <App /> 
+        return <div><App/>
             <br/><br/>
-                <Card style={{width:"60%", verticalAlign: 'center', margin: 'auto', padding: 10, display: 'block', backgroundColor: '#FF8A80', horizontalAlign: 'center'}}>
+            <Card style={{
+                width: "30%",
+                verticalAlign: 'center',
+                margin: 'auto',
+                padding: 5,
+                display: 'block',
+                backgroundColor: '#FF8A80',
+                horizontalAlign: 'center'
+            }}>
+                <strong>
                     <CardHeader>Register</CardHeader>
-                    <CardBody>
-                        <Form>
-                            <FormGroup row>
-                                <Label for="firstName" sm={2}>First Name</Label>
-                                <Col sm={10}>
-                                    <Input type="text" name="firstName" id="firstName" required/>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label for="lastName" sm={2}>Last Name</Label>
-                                <Col sm={10}>
-                                    <Input type="text" name="lastName" id="lastName" required/>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label for="teamName" sm={2}>Team Name</Label>
-                                <Col sm={10}>
-                                    <Input type="text" name="teamName" id="teamName" required/>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label for="email" sm={2}>Email</Label>
-                                <Col sm={10}>
-                                    <Input type="email" name="email" id="email" required/>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label for="password" sm={2}>Password</Label>
-                                <Col sm={10}>
-                                    <Input type="password" name="password" id="password" required/>
-                                </Col>
-                            </FormGroup>
-                            <Button color="primary" style={{ verticalAlign: 'center', margin: 'auto', padding: 10, display: 'block', horizontalAlign: 'center'}}>Register</Button>
-                        </Form>
-                    </CardBody>
-                </Card>
-  
+                </strong>
+                <CardBody>
+                    <Form style={{width: "120%"}}>
+                        <FormGroup row>
+                            <Label for="f_name" sm={5}>First Name</Label>
+                            <Col sm={10}>
+                                <Input type="text" name="f_name" id="f_name" required/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="l_name" sm={5}>Last Name</Label>
+                            <Col sm={10}>
+                                <Input type="text" name="l_name" id="l_name" required/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="t_name" sm={5}>Team Name</Label>
+                            <Col sm={10}>
+                                <Input type="text" name="t_name" id="t_name" required/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="email" sm={5}>Email</Label>
+                            <Col sm={10}>
+                                <Input type="email" name="email" id="email" required/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="password" sm={5}>Password</Label>
+                            <Col sm={10}>
+                                <Input type="password" name="password" id="password" required/>
+                            </Col>
+                        </FormGroup>
+                        <Button color="primary" style={{
+                            verticalAlign: 'center',
+                            margin: 'auto',
+                            padding: 10,
+                            display: 'block',
+                            horizontalAlign: 'center'
+                        }}>Sign in</Button>
+                    </Form>
+                </CardBody>
+            </Card>
         </div>
     }
 }
--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -1,37 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     Card, Button, CardHeader, CardFooter, CardBody,
-    CardTitle, CardText, Col, Row, Form, FormGroup, Label, Input, FormText
+    CardTitle, CardText, Col, Row, Form, FormGroup, Label, Input, FormText, FormFeedback
 } from 'reactstrap';
 import App from './App';
 
 export default class SignIn extends Component {
 
     render() {
-        return<div> <App />
-        <br/><br/>
-                <Card style={{width:"48%", verticalAlign: 'center', margin: 'auto', padding: 10, display: 'block', backgroundColor: '#FF8A80', horizontalAlign: 'center'}}>
+        return <div><App/>
+            <br/><br/>
+            <Card style={{
+                width: "30%",
+                verticalAlign: 'center',
+                margin: 'auto',
+                padding: 5,
+                display: 'block',
+                backgroundColor: '#FF8A80',
+                horizontalAlign: 'center'
+            }}>
+                <strong>
                     <CardHeader>Sign in</CardHeader>
-                    <CardBody>
-                        <Form>
-                            <FormGroup row>
-                                <Label for="email" sm={2}>Email</Label>
-                                <Col sm={10}>
-                                    <Input type="email" name="email" id="email" required/>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label for="password" sm={2}>Password</Label>
-                                <Col sm={10}>
-                                    <Input type="password" name="password" id="password" required/>
-                                </Col>
-                            </FormGroup>
-                            <Button color="primary" style={{ verticalAlign: 'center', margin: 'auto', padding: 10, display: 'block', horizontalAlign: 'center'}}>Sign in</Button>
-                        </Form>
-                    </CardBody>
-                </Card>
-                </div>
-    }
-}
-
-
+                </strong>
+                <CardBody>
+                    <Form style={{width: "120%"}}>
+                        <FormGroup row>
+                            <Label for="email" sm={5}>Email</Label>
+                            <Col sm={10}>
+                                <Input type="email" name="email" id="email" required/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="password" sm={5}>Password</Label>
+                            <Col sm={10}>
+                                <Input type="password" name="password" id="password" required/>
+                            </Col>
+                        </FormGroup>
+                        <Button color="primary" style={{
+                            width: "83.5%",
+                            verticalAlign: 'center', 
+                            margin: '0',
+                            padding: 10,
+                            display: 'block',
+                            horizontalAlign: 'center'
+                        }}>Sign in</Button>
+                    </Form>
+                </CardBody>
+            </Card>
+            < /div>
+                }
+                }
