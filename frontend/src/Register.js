c247da9023b0ec9101c01254576a4dae7bfef5af
rkej <rahulkejriwal18@gmail.com>
2019-07-21 15:00:05 -0400

    Added axios post request to send data to backend

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,15 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     Card, Button, CardHeader, CardFooter, CardBody,
     CardTitle, CardText, Col, Row, Form, FormGroup, Label, Input, FormText
 } from 'reactstrap';
+import axios from 'axios';
 import App from './App';
 
 export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            teamName: ''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
 
+    handleInputChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onFormSubmit=event=>{
+        event.preventDefault();
+        axios.post('/api/register', null, {
+            params: {
+                email: this.state.email,
+                password: this.state.password,
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                teamName: this.state.teamName
+            }
+        })
+    }
     render() {
-        return <div><App/>
-            <br/>
+        return <div><App />
+            <br />
             <Card style={{
                 width: "40%",
                 verticalAlign: 'center',
@@ -19,40 +52,44 @@ export default class SignIn extends Component {
                 backgroundColor: '#FFAB91',
                 horizontalAlign: 'center'
             }}>
-                
-                    <CardHeader style={{backgroundColor: '#FF1744', color: 'white'}}><strong>Register</strong></CardHeader>
-                
+
+                <CardHeader style={{ backgroundColor: '#FF1744', color: 'white' }}><strong>Register</strong></CardHeader>
+
                 <CardBody>
                     <Form>
                         <FormGroup>
                             <Label for="firstName">First Name</Label>
-                                <Input style={{width: "100%"}} type="text" name="firstName" id="firstName" required/>
+                            <Input onChange={this.handleInputChange} style={{ width: "100%" }} type="text" name="firstName" id="firstName" value={this.state.firstName} required/>
                         </FormGroup>
+
                         <FormGroup>
                             <Label for="lastName" >Last Name</Label>
-                                <Input style={{width: "100%"}} type="text" name="lastName" id="lastName" required/>
- 
+                            <Input onChange={this.handleInputChange} style={{ width: "100%" }} type="text" name="lastName" id="lastName" value={this.state.lastName} required />
                         </FormGroup>
+
                         <FormGroup>
                             <Label for="teamName" >Team Name</Label>
-                                <Input style={{width: "100%"}} type="text" name="teamName" id="teamName" required/>
+                            <Input onChange={this.handleInputChange} style={{ width: "100%" }} type="text" name="teamName" id="teamName" value={this.state.teamName} required />
                         </FormGroup>
+
                         <FormGroup>
                             <Label for="email" >Email</Label>
-                                <Input style={{width: "100%"}} type="email" name="email" id="email" required/>
+                            <Input onChange={this.handleInputChange} style={{ width: "100%" }} type="email" name="email" id="email" value={this.state.email} required />
                         </FormGroup>
+
                         <FormGroup>
                             <Label for="password" >Password</Label>
-                                <Input style={{width: "100%"}} type="password" name="password" id="password" required/>
+                            <Input onChange={this.handleInputChange} style={{ width: "100%" }} type="password" name="password" id="password" value={this.state.password} required />
                         </FormGroup>
-                        <Button color="primary" style={{
-                            verticalAlign: 'center',
-                            margin: 'auto',
-                            padding: 10,
-                            display: 'block',
-                            horizontalAlign: 'center'
-                        }}>Register</Button>
                     </Form>
+                    <Button color="primary" style={{
+                        verticalAlign: 'center',
+                        margin: 'auto',
+                        padding: 10,
+                        display: 'block',
+                        horizontalAlign: 'center'
+                    }} type="submit" onClick={this.onFormSubmit}>Register</Button>
+
                 </CardBody>
             </Card>
         </div>
