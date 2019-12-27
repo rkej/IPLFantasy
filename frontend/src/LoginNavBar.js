b017cbec129ca555f8bfed8b8a4c2c8bda67d9da
Rahul Kejriwal <rahulkejriwal18@gmail.com>
2019-12-27 06:45:58 -0500

    user home created

---
+++ frontend/src/LoginNavBar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink } from 'reactstrap';
+
+export default class LoginNavBar extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+      }
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
+
+    render() {
+        const { classes } = this.props;
+        return <div> <Navbar style = {{backgroundColor: "#7F3F98"}} dark expand ="md">
+            <NavbarBrand>CricFantasy</NavbarBrand>
+            <NavbarToggler onClick={this.toggle}/>
+            <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className = "ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href = "/dashboard">Dashboard</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href = "/rules">Scoring rules</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href = "/">Logout</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+                 </Navbar>
+            </div>
+    }
+}
